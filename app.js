8b5ba714323bdb8b7b48296528901f3f5d1b6633
zahraghasemip <zahraghasemi2487@gmail.com>
2022-04-23 11:24:12 +0430

    just for test

--- app.js
+++ app.js
@@ -3,11 +3,11 @@
 alert("welcome to my page \n please answer questions in the field");
 const firstname=prompt("what is your firstname?");
 const lastname=prompt("what is your lastname?");
-const gender=prompt("what is your gender?(F/M/unbinary)");
+const gender=prompt("what is your gender?(F/M/unbinary) ");
 alert(firstname + ' ' +lastname  + ' ' + gender);
 const number1=prompt("please enter your first number");
 const number2=prompt("please enter your second number");
-
+  
 alert (number1 +'&'+number2);
 alert (number2 +'&'+number1);
 
