25393c06268d19a2d39435d39ac92d3b5c1c1f59
zahraghasemip <zahraghasemi2487@gmail.com>
2022-04-24 09:00:36 +0430

    upload math/js project

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 "use strict"
+function rand(x,y)
+{
 
-alert("welcome to my page \n please answer questions in the field");
-const firstname=prompt("what is your firstname?");
-const lastname=prompt("what is your lastname?");
-const age=prompt("enter your age");
-const gender=prompt("what is your gender?(F/M/unbinary) ");
+    return y+Math.floor(Math.random()*(x-y));
+} 
+console.log(rand(1,10));
+/////////
+function bin2dec(str) { 
+   let binary="1010101";
+   return parseInt(binary, 2);
+}
+console.log(bin2dec("10101010"));
+
+// alert("welcome to my page \n please answer questions in the field");
+// const firstname=prompt("what is your firstname?");
+// const lastname=prompt("what is your lastname?");
+// const age=prompt("enter your age");
+// const gender=prompt("what is your gender?(F/M/unbinary) ");
 // alert(firstname + ' ' +lastname  + ' ' + gender);
 // const number1=prompt("please enter your first number");
 // const number2=prompt("please enter your second number");
@@ -12,9 +24,9 @@ const gender=prompt("what is your gender?(F/M/unbinary) ");
 // alert (number1 +'&'+number2);
 // alert (number2 +'&'+number1);
 
-alert(
-    `name: ${firstname}`+" "+`lastname: ${lastname}`+" "+`age: ${age}`+" "+`gender: ${gender}`
-)
+// alert(
+//     `name: ${firstname}`+" "+`lastname: ${lastname}`+" "+`age: ${age}`+" "+`gender: ${gender}`
+// )
 
 
 
